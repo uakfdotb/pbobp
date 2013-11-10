c754cd9d609b0d181555da1b831f7ce87a96c5ac
Favyen Bastani <fbastani@perennate.com>
2013-11-09 20:40:46 -0500

    Add main cron.php file.

---
+++ public_html/cron.php
@@ -0,0 +1,35 @@
+<?php
+/*
+
+	pbobp
+	Copyright [2013] [Favyen Bastani]
+
+	This file is part of the pbobp source code.
+
+	pbobp is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	pbobp source code is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with pbobp source code. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+if(php_sapi_name() !== 'cli') {
+	die('Access denied.');
+}
+
+include("include/include.php");
+include("include/cron.php");
+
+cron_generate_invoices();
+cron_end_overdue_services();
+plugin_call('cron');
+
+?>
